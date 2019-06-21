5b7ef8aada8d3eed1cfd21cc58f791a86740c4bf
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-06-21 17:45:37 +0200

    add routes for users page

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import SideMenu from "./components/SideMenu";
 import MainMenu from "./components/MainMenu";
+import Users from "./components/Users";
 
 const AdminPanel = () => {
   return (
-    <div>
-      <SideMenu />
-      <MainMenu />
-      Hello world
-    </div>
+    <Router>
+      <div>
+        <SideMenu />
+        <Route path="/" exact component={MainMenu} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
   );
 };
 
