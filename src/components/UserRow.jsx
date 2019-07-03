ba94adf2d787793078b498dd25b06c67a96374ee
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-03 09:56:55 +0200

    changing variable name to match back

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const UserRow = ({ name, id, password, token, role, deleteUser }) => {
+const UserRow = ({ name, uuid, password, token, role, deleteUser }) => {
   return (
     <div>
       <h1>
-        Name : <Link to={`/user/${id}`}>{name}</Link>
+        Name : <Link to={`/user/${uuid}`}>{name}</Link>
       </h1>
-      <p>Son id est : {id}</p>
+      <p>Son id est : {uuid}</p>
       <p>Son mot de pass est : {password}</p>
       <p>cet utilisateur est un {role}</p>
       <p>Son Token est : {token}</p>
       <button
         style={{ background: "red", color: "white" }}
-        onClick={() => deleteUser(id)}
+        onClick={() => deleteUser(uuid)}
       >
         DELETE
       </button>
