5949d9bd6631393d3b48551abee3be452d54ca40
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-03 09:59:55 +0200

    add button to go back to users page

--- src/pages/User.page.jsx
+++ src/pages/User.page.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import UserInfoContainer from "../pills/userInfo/userInfo.container";
+import { Link } from "react-router-dom";
 
-const UserPage = ({ match }) => {
+const UserPage = ({ match, history }) => {
   return (
     <div>
+      <Link to="/users">
+        <button>Revenir aux utilisateurs</button>
+      </Link>
       <UserInfoContainer id={match.params.id} />
     </div>
   );
