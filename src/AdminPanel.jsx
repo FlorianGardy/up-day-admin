9582de9412735ea4d7c33602591cdf05bacacecc
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-08 16:27:21 +0200

    remove side menu and add login page

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import SideMenu from "./components/SideMenu";
 import Main from "./components/Main";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const AdminPanel = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <Router>
-        <SideMenu />
-        <Main />
-      </Router>
-    </div>
+    <Router>
+      <Main />
+    </Router>
   );
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import UserPage from "../pages/Users.page";
 import UserPageByID from "../pages/User.page";
-import Home from "./Home";
 import { Route } from "react-router-dom";
+import LoginForm from "../pages/login.page";
 
 const Main = () => {
   return (
-    <div style={{ width: "80%" }}>
-      <Route path="/" exact component={Home} />
+    <div>
+      <Route path="/" exact component={LoginForm} />
       <Route path="/users" exact component={UserPage} />
       <Route path="/user/:id" exact component={UserPageByID} />
     </div>
