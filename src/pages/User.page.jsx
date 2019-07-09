f45bb16b1ccf117612dd72a8beb51d882bbb757e
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-09 15:45:55 +0200

    use semantic Button on back to users

--- src/pages/User.page.jsx
+++ src/pages/User.page.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import UserInfoContainer from "../pills/userInfo/userInfo.container";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Button } from "semantic-ui-react";
 
 const UserPage = ({ match }) => {
   return (
     <div>
       <Link to="/users">
-        <button>Revenir aux utilisateurs</button>
+        <Button>Revenir aux utilisateurs</Button>
       </Link>
       <UserInfoContainer uuid={match.params.uuid} />
     </div>
