50d4b5c98b9775d40df57a7e2af86e35637593af
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-09 17:40:08 +0200

    adding private routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginForm from "./pages/login.page";
 import UserPage from "./pages/Users.page";
 import UserPageByID from "./pages/User.page";
+import PrivateRoute from "./components/PrivateRoute";
 
 const AdminPanel = () => {
   return (
     <Router>
       <div style={{ width: "80%", margin: "auto" }}>
         <Route path="/login" exact component={LoginForm} />
-        <Route path="/" exact component={UserPage} />
-        <Route path="/users" exact component={UserPage} />
-        <Route path="/user/:uuid" exact component={UserPageByID} />
+        <PrivateRoute path="/" exact component={UserPage} />
+        <PrivateRoute path="/users" exact component={UserPage} />
+        <PrivateRoute path="/user/:uuid" exact component={UserPageByID} />
       </div>
     </Router>
   );
