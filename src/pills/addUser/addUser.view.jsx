1e34b8e3c89f785363d5bc596f4946fd2e2b64cb
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-09 16:50:41 +0200

    change adduser input form

--- src/pills/addUser/addUser.view.jsx
+++ src/pills/addUser/addUser.view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Input } from "semantic-ui-react";
 
 const AddUserView = ({
   name,
@@ -15,31 +16,33 @@ const AddUserView = ({
 }) => {
   return (
     <form onSubmit={() => handleRegister(name, password, role, email)}>
-      <input
+      <Input
+        placeholder="Nom d'utilisateur"
         type="text"
-        placeholder="Login"
         value={name}
         onChange={e => updateName(e.target.value)}
       />
-      <input
+
+      <Input
         type="password"
         placeholder="Mot de passe"
         value={password}
         onChange={e => updatePass(e.target.value)}
       />
-      <input
+      <Input
         type="email"
         placeholder="E-mail"
         value={email}
         onChange={e => updateEmail(e.target.value)}
       />
-      <label>admin</label>
-      <input
+      <Input type="submit" />
+      <label>Admin ?</label>
+      <Input
+        color="red"
         type="checkbox"
         onChange={e => updateAdmin(e.target.checked)}
         checked={isAdmin}
       />
-      <input type="submit" />
     </form>
   );
 };
