0a2d408ddb4b479f747abd91a07b769a74eaf5cc
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-14 21:33:48 +0200

    change welcome text

--- src/pills/login/login.view.jsx
+++ src/pills/login/login.view.jsx
@@ -19,7 +19,7 @@ const LoginView = ({ history, onSubmit, statusCode }) => {
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
         <Header as="h2" color="orange" textAlign="center">
-          Se connecter
+          Bienvenue
         </Header>
         <Form size="large" onSubmit={e => onSubmit(e)}>
           <Segment>
