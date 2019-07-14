328a9d16bc497d29ee9fc0ccf997981483068dc2
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-14 22:03:19 +0200

    remove ; in <App />

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Router from "./Router";
-import { Container } from 'semantic-ui-react';
-
+import { Container } from "semantic-ui-react";
 
 function App() {
   return (
-  <Container fluid>
-    <Router />;
-  </Container>
-  )
+    <Container fluid>
+      <Router />
+    </Container>
+  );
 }
 
 export default App;
