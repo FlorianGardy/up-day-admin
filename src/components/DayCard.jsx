0cf2eaca065d1647944dd6d10ebef3b8d7d5bdc3
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-14 22:05:26 +0200

    add s when more than 1 nature

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import { Card } from "semantic-ui-react";
 import EventList from "./EventList";
 
-
-
-
 const DayCard = ({ day }) => {
-	const { date, events, natureCount } = day;
+  const { date, events, natureCount } = day;
 
-	const natureEntries = Object.entries(natureCount);
+  const natureEntries = Object.entries(natureCount);
 
-	return (
-		<Card fluid style={{border:"1px solid #F2711C"}}>
-      <Card.Content textAlign="center" style={{fontWeight:"bold"}}>{date}</Card.Content>
-      <Card.Content textAlign="center">
-			{natureEntries.map((n, i) => (
-				<div key={i}>
-					{n[1]} {n[0]}
-				</div>
-      ))}
+  return (
+    <Card fluid style={{ border: "1px solid #F2711C" }}>
+      <Card.Content textAlign="center" style={{ fontWeight: "bold" }}>
+        {date}
       </Card.Content>
-			<Card.Content>
-				<EventList events={events} />
-			</Card.Content>
-		</Card>
-	);
+      <Card.Content textAlign="center">
+        {natureEntries.map((n, i) => (
+          <div key={i}>
+            {n[1]} {n[0]}
+            {n[1] > 1 && "s"}
+          </div>
+        ))}
+      </Card.Content>
+      <Card.Content>
+        <EventList events={events} />
+      </Card.Content>
+    </Card>
+  );
 };
 export default DayCard;
