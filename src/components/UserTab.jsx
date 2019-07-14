8e126a99998f90caeb032a44f57d6fa270e5913c
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-14 21:41:56 +0200

    remove require proptype email in userTab

--- src/components/UserTab.jsx
+++ src/components/UserTab.jsx
@@ -6,43 +6,49 @@ import moment from "moment";
 import "moment/locale/fr";
 
 const UserTab = ({
-	history,
-	name,
-	uuid,
-	password,
-	createdAt,
-	token,
-	role,
-	deleteUser,
-	email
+  history,
+  name,
+  uuid,
+  password,
+  createdAt,
+  token,
+  role,
+  deleteUser,
+  email
 }) => {
-	return (
-		<Table.Body>
-			<Table.Row textAlign="center">
-				<Table.Cell>
-					<NavLink to={`/user/${uuid}`}>
-							<Icon name="calendar alternate outline" size ="large" color="grey"></Icon>
-					</NavLink>
-				</Table.Cell>
-				<Table.Cell>{name}</Table.Cell>
-				<Table.Cell>{moment(createdAt).format("DD/MM/YYYY")}</Table.Cell>
-				<Table.Cell>{email ? email : "non communiqué"}</Table.Cell>
-				<Table.Cell>
-          <Icon link size ="large" name="trash alternate outline" onClick={() => deleteUser(uuid)} color="orange"></Icon>
-				</Table.Cell>
-			</Table.Row>
-		</Table.Body>
-	);
+  return (
+    <Table.Body>
+      <Table.Row textAlign="center">
+        <Table.Cell>
+          <NavLink to={`/user/${uuid}`}>
+            <Icon name="calendar alternate outline" size="large" color="grey" />
+          </NavLink>
+        </Table.Cell>
+        <Table.Cell>{name}</Table.Cell>
+        <Table.Cell>{moment(createdAt).format("DD/MM/YYYY")}</Table.Cell>
+        <Table.Cell>{email ? email : "non communiqué"}</Table.Cell>
+        <Table.Cell>
+          <Icon
+            link
+            size="large"
+            name="trash alternate outline"
+            onClick={() => deleteUser(uuid)}
+            color="orange"
+          />
+        </Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  );
 };
 
 UserTab.propTypes = {
-	name: PropTypes.string.isRequired,
-	email: PropTypes.string.isRequired,
-	uuid: PropTypes.string.isRequired,
-	password: PropTypes.string.isRequired,
-	token: PropTypes.string.isRequired,
-	role: PropTypes.string.isRequired,
-	deleteUser: PropTypes.func.isRequired
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string,
+  uuid: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  token: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  deleteUser: PropTypes.func.isRequired
 };
 
 export default UserTab;
