d37eac374a285f0fd57af2f9810330e5d590ed41
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-15 23:17:49 +0200

    [review] change getName to getNameSelector extract text from render

--- src/components/TitleMenu.jsx
+++ src/components/TitleMenu.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
-import { getName } from "../pills/login/login.selector";
 import { Menu, Icon } from "semantic-ui-react";
 
-const TitleMenu = ({name}) => {
-		return (
-			<Menu inverted>
-        <Menu.Item header>
-          <Icon name="settings" />
-            UP Day Administration
-        </Menu.Item>
-				<Menu.Item name="AdminUser" position="right">
-          <Icon circular inverted color="orange" name="user md" />
-          {name}
-        </Menu.Item>
-			</Menu>
-		);
-}
+import { getNameSelector } from "../pills/login/login.selector";
 
-const mapStateToProps = state => {
-	return {
-		name: getName(state)
-	};
+const _title = "UP Day Administration";
+
+const TitleMenu = ({ name }) => {
+  return (
+    <Menu inverted>
+      <Menu.Item header>
+        <Icon name="settings" />
+        {_title}
+      </Menu.Item>
+      <Menu.Item name="AdminUser" position="right">
+        <Icon circular inverted color="orange" name="user md" />
+        {name}
+      </Menu.Item>
+    </Menu>
+  );
 };
 
+const mapStateToProps = state => {
+  return {
+    name: getNameSelector(state)
+  };
+};
 
 export default connect(mapStateToProps)(TitleMenu);
