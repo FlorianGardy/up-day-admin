2b02806e55e9c5ca3158c201fedf3ececec14d70
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-15 22:53:20 +0200

    [review] add PropTypes shapes for {day} extract styles from render rewrite count and label display separate imports

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
+import PropTypes from "prop-types";
+
 import EventList from "./EventList";
+
+const _cardStyle = { border: "1px solid #F2711C" };
+const _cardContentStyle = { fontWeight: "bold" };
 
 const DayCard = ({ day }) => {
   const { date, events, natureCount } = day;
-
   const natureEntries = Object.entries(natureCount);
 
   return (
-    <Card fluid style={{ border: "1px solid #F2711C" }}>
-      <Card.Content textAlign="center" style={{ fontWeight: "bold" }}>
+    <Card fluid style={_cardStyle}>
+      <Card.Content textAlign="center" style={_cardContentStyle}>
         {date}
       </Card.Content>
       <Card.Content textAlign="center">
-        {natureEntries.map((n, i) => (
-          <div key={i}>
-            {n[1]} {n[0]}
-            {n[1] > 1 && "s"}
-          </div>
+        {natureEntries.map(([label, count], i) => (
+          <div key={i}>{`${count} ${label}${count > 1 ? "s" : ""}`}</div>
         ))}
       </Card.Content>
       <Card.Content>
@@ -26,4 +27,25 @@ const DayCard = ({ day }) => {
     </Card>
   );
 };
+
+DayCard.propTypes = {
+  day: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    events: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        date: PropTypes.string.isRequierd,
+        type: PropTypes.string.isRequierd,
+        nature: PropTypes.string.isRequierd,
+        volume: PropTypes.string.isRequierd,
+        context: PropTypes.arrayOf(PropTypes.string),
+        comment: PropTypes.string,
+        uuid: PropTypes.string.isRequierd
+      })
+    ).isRequired,
+    natureCount: PropTypes.objectOf(PropTypes.number)
+      .isRequired /* keys are dynamics here */
+  }).isRequired
+};
+
 export default DayCard;
