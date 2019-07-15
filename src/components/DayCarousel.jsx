7d077e5b0aaaf5d5fac5f96db4847e13a66a5a7c
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-15 09:07:31 +0200

    sort events in day carousel

--- src/components/DayCarousel.jsx
+++ src/components/DayCarousel.jsx
@@ -7,50 +7,48 @@ import CarouselButton from "./CarouselButton";
 import { CardGroup } from "semantic-ui-react";
 
 const carousel = {
-	display: "grid",
-	gridTemplateColumns: "3% 94% 3%"
-}
-
-
+  display: "grid",
+  gridTemplateColumns: "3% 94% 3%"
+};
 
 const DayCarousel = ({ events }) => {
-	const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(events.length + 1);
 
-	return (
-		<div style={carousel}>
-				<CarouselButton
-					icon="chevron left"
-					onClick={() => setCurrentIndex(Math.max(0, currentIndex - 1))}
-				/>
-				<CardGroup centered itemsPerRow={3}>
-			{events
-				.filter(
-					day =>
-						events.indexOf(day) < currentIndex + 3 &&
-						events.indexOf(day) >= currentIndex
-				)
-				.map((day, i) => (
-					<DayCard key={i} day={day} />
-				))}
-				</CardGroup>
-			<CarouselButton
-				icon="chevron right"
-				onClick={() =>
-					setCurrentIndex(Math.min(events.length - 3, currentIndex + 1))
-				}
-			/>
-		</div>
-	);
+  return (
+    <div style={carousel}>
+      <CarouselButton
+        icon="chevron left"
+        onClick={() => setCurrentIndex(Math.max(0, currentIndex - 1))}
+      />
+      <CardGroup centered itemsPerRow={3}>
+        {events
+          .filter(
+            day =>
+              events.indexOf(day) < currentIndex + 3 &&
+              events.indexOf(day) >= currentIndex
+          )
+          .map((day, i) => (
+            <DayCard key={i} day={day} />
+          ))}
+      </CardGroup>
+      <CarouselButton
+        icon="chevron right"
+        onClick={() =>
+          setCurrentIndex(Math.min(events.length - 3, currentIndex + 1))
+        }
+      />
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
-	return {
-		userEvents: getUserEvents(state)
-	};
+  return {
+    userEvents: getUserEvents(state)
+  };
 };
 
 DayCarousel.propTypes = {
-	userEvents: PropTypes.array.isRequired
+  userEvents: PropTypes.array.isRequired
 };
 
 export default connect(mapStateToProps)(DayCarousel);
