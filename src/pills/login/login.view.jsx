47096ecda3bd5deb524ccb46957964992366fa8d
Perrine Martyris <perrinemartyris@gmail.com>
2019-07-15 13:21:52 +0200

    finished the login page style

--- src/pills/login/login.view.jsx
+++ src/pills/login/login.view.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Button, Form, Grid, Header, Segment } from "semantic-ui-react";
+import { Button, Form, Grid, Header, Segment, Icon, Image } from "semantic-ui-react";
 
-const LoginView = ({ history, onSubmit, statusCode }) => {
+import logoKineOrange from "../../assets/logoKineOrange.png";
+import HeaderSubHeader from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
+
+const LoginView = ({ onSubmit, statusCode }) => {
   let styled = { color: "red" };
   styled.opacity = statusCode !== 200 ? 1 : 0;
   let errorMessage;
@@ -15,11 +18,15 @@ const LoginView = ({ history, onSubmit, statusCode }) => {
     default:
       errorMessage = "";
   }
+
   return (
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
         <Header as="h2" color="orange" textAlign="center">
-          Bienvenue
+          <Image src={logoKineOrange} alt="logo" style={{width:"30%"}} />
+          <h1 style={{fontSize:"3rem", fontWeight:"bold", color:"#243330"}}>UP Day Admin</h1>
+        <Header.Subheader style={{fontSize:"1.5rem"}}>Bienvenue sur votre espace administrateur</Header.Subheader>
+        <br/>
         </Header>
         <Form size="large" onSubmit={e => onSubmit(e)}>
           <Segment>
@@ -38,7 +45,7 @@ const LoginView = ({ history, onSubmit, statusCode }) => {
               placeholder="Mot de passe"
               type="password"
             />
-            <Button color="orange" fluid size="large" type="submit">
+            <Button style={{backgroundColor:"#243330"}} inverted fluid size="large" type="submit">
               Se connecter
             </Button>
           </Segment>
