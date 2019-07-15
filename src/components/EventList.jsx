57db48359f1479831b900bf7ae045a1e2cae009e
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-15 23:07:05 +0200

    [review] transform DeleteModal from Class to Stateless add proptypes extract text from render

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,24 +1,39 @@
 import React from "react";
-import EventCard from "./EventCard";
 import PropTypes from "prop-types";
+
+import EventCard from "./EventCard";
+
+const _style = {
+  height: "45vMin",
+  overflow: "auto"
+};
 
 const EventList = ({ events }) => {
   return (
-    <div
-      style={{
-        height: "45vMin",
-        overflow: "auto"
-      }}
-    >
-      {events.map((event, i) => (
-        <EventCard key={i} {...event} />
+    <div style={_style}>
+      {events.map(({ date, type, nature, context, volume, comment }, i) => (
+        <EventCard
+          key={i}
+          {...{ date, type, nature, context, volume, comment }}
+        />
       ))}
     </div>
   );
 };
 
 EventList.propTypes = {
-  events: PropTypes.array.isRequired
+  events: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      date: PropTypes.string.isRequierd,
+      type: PropTypes.string.isRequierd,
+      nature: PropTypes.string.isRequierd,
+      volume: PropTypes.string.isRequierd,
+      context: PropTypes.arrayOf(PropTypes.string),
+      comment: PropTypes.string,
+      uuid: PropTypes.string.isRequierd
+    })
+  ).isRequired
 };
 
 export default EventList;
