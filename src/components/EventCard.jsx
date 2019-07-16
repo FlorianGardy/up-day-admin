6e9e5a1f74d857a0ae081168287d9bb57da5e2b4
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-16 08:55:24 +0200

    extract style from render

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -4,12 +4,13 @@ import { List } from "semantic-ui-react";
 import "moment/locale/fr";
 
 const EventCard = ({ date, type, nature, context, volume, comment }) => {
+  const headerStyle = { display: "flex", justifyContent: "space-between" };
+  const contextStyle = { display: "flex", justifyContent: "space-evenly" };
+  const commentStyle = { color: "teal", fontStyle: "italic" };
   return (
     <List divided>
       <List.Item>
-        <List.Header
-          style={{ display: "flex", justifyContent: "space-between" }}
-        >
+        <List.Header style={headerStyle}>
           <div>{date}</div>
           <div>{nature}</div>
           <div>{type}</div>
@@ -19,18 +20,14 @@ const EventCard = ({ date, type, nature, context, volume, comment }) => {
       <List.Item>
         <List.Content>
           {context && (
-            <List.Description
-              style={{ display: "flex", justifyContent: "space-evenly" }}
-            >
+            <List.Description style={contextStyle}>
               {context.map((el, i) => (
                 <div key={i}>{el}</div>
               ))}
             </List.Description>
           )}
           {comment && (
-            <List.Description style={{ color: "teal", fontStyle: "italic" }}>
-              {comment}
-            </List.Description>
+            <List.Description style={commentStyle}>{comment}</List.Description>
           )}
         </List.Content>
       </List.Item>
