69af019f49f2af052d324721d4e0a6a51f643d0a
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-17 15:08:09 +0200

    extract style and text from render

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -4,96 +4,100 @@ import PropTypes from "prop-types";
 
 import EventList from "./EventList";
 
-const _cardStyle = {
-	minHeight: "max-content",
-	backgroundColor: "rgba(212,212,212,.3)",
-	padding: "0 5px",
-	display: "flex",
-	flexDirection: "column",
-	alignContent: "top"
+const cardStyle = {
+  minHeight: "max-content",
+  backgroundColor: "rgba(212,212,212,.3)",
+  padding: "0 5px",
+  display: "flex",
+  flexDirection: "column",
+  alignContent: "top"
 };
-const _dateContentStyle = {
-	fontWeight: "bold",
-	color: "#243330",
-	maxHeight: "max-content"
+const dateContentStyle = {
+  fontWeight: "bold",
+  color: "#243330",
+  maxHeight: "max-content"
 };
-const _recapContentStyle = {
-	padding: "10px 0",
-	maxHeight: "min-content"
+const recapContentStyle = {
+  padding: "10px 0",
+  maxHeight: "min-content"
 };
-const _tableStyle = {
-	padding: "0",
-	maxWidth: "100%",
-	backgroundColor: "rgba(36,51, 48, 1)"
+const tableStyle = {
+  padding: "0",
+  maxWidth: "100%",
+  backgroundColor: "rgba(36,51, 48, 1)"
 };
-const _eventStyle = {
-	padding: "0"
+const eventStyle = {
+  padding: "0"
 };
+
+const TITLE = "Récapitulatif";
 
 const DayCard = ({ day }) => {
-	const { date, events, natureCount } = day;
-	const natureEntries = Object.entries(natureCount);
+  const { date, events, natureCounter } = day;
+  const natureEntries = Object.entries(natureCounter);
 
-	return (
-		<Card fluid centered style={_cardStyle}>
-			<Card.Content textAlign="center" style={_dateContentStyle}>
-				{date}
-			</Card.Content>
-			<Card.Content textAlign="center" style={_recapContentStyle}>
-				<Table
-					celled
-					style={_tableStyle}
-					compact
-					size="small"
-					textAlign="center"
-					inverted
-				>
-					<Table.Header>
-						<Table.Row>
-							<Table.HeaderCell colSpan="3">Récapitulatif</Table.HeaderCell>
-						</Table.Row>
-					</Table.Header>
-					<Table.Body>
-						<Table.Row>
-							{natureEntries.map(([label, count], i) => (
-								<Table.Cell key={i}>{`${label}${
-									count > 1 ? "s" : ""
-								}`}</Table.Cell>
-							))}
-						</Table.Row>
-						<Table.Row>
-							{natureEntries.map(([label, count], i) => (
-								<Table.Cell key={i}>{count}</Table.Cell>
-							))}
-						</Table.Row>
-					</Table.Body>
-				</Table>
-			</Card.Content>
-			<Card.Content style={_eventStyle}>
-				<EventList events={events} />
-			</Card.Content>
-		</Card>
-	);
+  return (
+    <Card fluid centered style={cardStyle}>
+      <Card.Content textAlign="center" style={dateContentStyle}>
+        {date}
+      </Card.Content>
+      <Card.Content textAlign="center" style={recapContentStyle}>
+        <Table
+          celled
+          style={tableStyle}
+          compact
+          size="small"
+          textAlign="center"
+          inverted
+        >
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell colSpan={natureEntries.length}>
+                {TITLE}
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              {natureEntries.map(([label, count], i) => (
+                <Table.Cell key={i}>{`${label}${
+                  count > 1 ? "s" : ""
+                }`}</Table.Cell>
+              ))}
+            </Table.Row>
+            <Table.Row>
+              {natureEntries.map(([label, count], i) => (
+                <Table.Cell key={i}>{count}</Table.Cell>
+              ))}
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </Card.Content>
+      <Card.Content style={eventStyle}>
+        <EventList events={events} />
+      </Card.Content>
+    </Card>
+  );
 };
 
 DayCard.propTypes = {
-	day: PropTypes.shape({
-		date: PropTypes.string.isRequired,
-		events: PropTypes.arrayOf(
-			PropTypes.shape({
-				id: PropTypes.number.isRequired,
-				date: PropTypes.string.isRequierd,
-				type: PropTypes.string.isRequierd,
-				nature: PropTypes.string.isRequierd,
-				volume: PropTypes.string.isRequierd,
-				context: PropTypes.arrayOf(PropTypes.string),
-				comment: PropTypes.string,
-				uuid: PropTypes.string.isRequierd
-			})
-		).isRequired,
-		natureCount: PropTypes.objectOf(PropTypes.number)
-			.isRequired /* keys are dynamics here */
-	}).isRequired
+  day: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    events: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        date: PropTypes.string.isRequierd,
+        type: PropTypes.string.isRequierd,
+        nature: PropTypes.string.isRequierd,
+        volume: PropTypes.string.isRequierd,
+        context: PropTypes.arrayOf(PropTypes.string),
+        comment: PropTypes.string,
+        uuid: PropTypes.string.isRequierd
+      })
+    ).isRequired,
+    natureCounter: PropTypes.objectOf(PropTypes.number)
+      .isRequired /* keys are dynamics here */
+  }).isRequired
 };
 
 export default DayCard;
